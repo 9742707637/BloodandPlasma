1f0df52ff7bee5a7537c977ec5974a8df26f95b6
Praveen Shankar <57632509+PraveenDShankar@users.noreply.github.com>
2024-12-19 13:40:40 +0530

    Create Usingeksctl.sh

---
+++ kubernestes/Usingeksctl.sh
@@ -0,0 +1,11 @@
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+# install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+# create eks cluster
+eksctl create cluster --name my-clusterkube --region us-east-1
